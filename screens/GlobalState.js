01c36499f0880d5a66b03f1dbcce8ea56c5391d9
aymanelfadl <aymanelfadl16@gmail.com>
2024-06-12 22:28:13 +0200

    fix icons home

--- screens/GlobalState.js
+++ screens/GlobalState.js
@@ -1,22 +1,21 @@
 // GlobalState.js
 let globalState = {
-    user: null,
-
-    serverLink:"http://192.168.11.240:8000/",
+  user: null,
+  serverLink: "http://192.168.1.105:8000/",
 };
 
 export const setUser = (user) => {
-    return new Promise((resolve) => {
-        console.log("ill set : "+user);
-        globalState.user = user;
-        resolve();
-    });
+  return new Promise((resolve) => {
+    console.log("ill set : " + user);
+    globalState.user = user;
+    resolve();
+  });
 };
 
 export const getUser = () => {
-    console.log("ill return  : "+ globalState.user)
-    return globalState.user;
+  console.log("ill return  : " + globalState.user);
+  return globalState.user;
 };
 export const getLink = () => {
-    return globalState.serverLink;
+  return globalState.serverLink;
 };
